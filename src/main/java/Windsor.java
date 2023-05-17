31e84a5a521968f2871f2dd0cf2818a0cfcee0a3
KnudNielsen999 <88492416+KnudNielsen999@users.noreply.github.com>
2023-05-17 10:19:09 +0200

    Maven 17/5 version 2

--- src/main/java/Windsor.java
+++ src/main/java/Windsor.java
@@ -8,7 +8,6 @@ import java.util.List;
 
 public class Windsor {
     public static void main(String[] args) {
-        List<LagerModel> lagerListe = new ArrayList<>();
 
         System.out.println("Windsor ERP System\n");
 
@@ -16,11 +15,8 @@ public class Windsor {
         lm1.setVarenummer("1");
         lm1.setVareMaengde(10);
         lm1.setKostPris(10.5);
-        lagerListe.add(lm1);
 
-       /* for (LagerModel lm : lagerListe) {
-            System.out.println(lm);
-        }*/
+
         System.out.println("\n");
         lm1.varenummerVisning();
         lm1.lagerTransaktioner(lm1,2);
